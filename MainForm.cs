a03a5b61154fdc77aceb21a4886b03745c054608
pixochi <kozak.jakub55@gmail.com>
2019-09-29 19:23:18 +1000

    Add a separate GA service

--- MainForm.cs
+++ MainForm.cs
@@ -64,7 +64,7 @@ namespace GUI
 
                 this.printRow();
                 this.printRow("From Service:");
-                var client = new HeuristicServiceReference.HeuristicServiceClient();
+                var client = new GAServiceReference.GAServiceClient();
                 var allocInput = new TaskAllocationInput(configFile.Tasks, configFile.Processors, configFile.MaxDuration, configFile.RefFrequency, configFile.Coefficients);
                 List<TaskAllocationOutput> allocations = client.GetAllocations(allocInput);
 
